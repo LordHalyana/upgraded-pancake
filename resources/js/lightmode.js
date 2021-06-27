752066d387c5a6a6ca3f9ce296aeb5780d96ea78
LordHalyana <1707310+LordHalyana@users.noreply.github.com>
2021-06-27 20:33:50 +0200

    added lightmode button for boomers

---
+++ resources/js/lightmode.js
@@ -0,0 +1,7 @@
+function lightmode() {
+    var element = document.body;
+    element.classList.toggle("lightmode");
+
+    var t1 = document.getElementById("drkbttn")
+    t1.classList.toggle("darkmode");
+  }
